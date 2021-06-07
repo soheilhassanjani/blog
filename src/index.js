74514a09f1b4744092b12ff342dae1aaaecc32ae
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-08 02:52:29 +0430

    Add Some provider For Congif Lib

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import "antd/dist/antd.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import LoginProvider from "Provider/login/login.provider";
+import { QueryClient, QueryClientProvider } from "react-query";
+import RegisterProvider from "Provider/register/register.provider";
+import { ConfigProvider } from "antd";
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ConfigProvider direction="rtl">
+        <QueryClientProvider client={queryClient}>
+          <LoginProvider>
+            <RegisterProvider>
+              <App />
+            </RegisterProvider>
+          </LoginProvider>
+        </QueryClientProvider>
+      </ConfigProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
