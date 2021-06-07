309854fcd47ce6dcbd195b2947bbb30e11aa2bfd
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-08 02:54:21 +0430

    Add routes app

--- src/Pages/app/index.js
+++ src/Pages/app/index.js
@@ -1,8 +1,32 @@
-import React from "react";
-import { Switch } from "react-router";
+import React, { lazy, Suspense } from "react";
+import { Redirect, Switch } from "react-router";
+import AppLayout from "Layout/AppLayout";
+import PrivateRoute from "Routes/PrivateRoute";
+import Loading from "Components/Loading/Loading";
+
+const Article = lazy(() => import("Pages/app/Article"));
+const AddArticle = lazy(() => import("Pages/app/AddArticle"));
+const Category = lazy(() => import("Pages/app/Category"));
+const AddCategory = lazy(() => import("Pages/app/AddCategory"));
 
 function Index() {
-  return <Switch>app</Switch>;
+  return (
+    <AppLayout>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Redirect exact from="/app" to="/app/article" />
+          <PrivateRoute path="/app/article" component={Article} to="/" />
+          <PrivateRoute path="/app/add-article" component={AddArticle} to="/" />
+          <PrivateRoute path="/app/category" component={Category} to="/" />
+          <PrivateRoute
+            path="/app/add-category"
+            component={AddCategory}
+            to="/"
+          />
+        </Switch>
+      </Suspense>
+    </AppLayout>
+  );
 }
 
 export default Index;
