de7d0b915c1069a7b013edbb1f53b5d5df722e42
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-08 02:56:22 +0430

    refactor Routes

--- src/Routes/GuestRoute.js
+++ src/Routes/GuestRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ConditionalRoute from './ConditionalRoute';
 const GuestRoute = (props) => {
-  const predicate = localStorage.getItem('user');
+  const predicate = localStorage.getItem('AUTH_TOKEN');
   return <ConditionalRoute predicate={!Boolean(predicate)} {...props} />;
 };
 export default GuestRoute;
