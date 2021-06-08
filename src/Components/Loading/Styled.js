913ca0ce820bf06a1841416be4f0079068c8654a
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-09 01:21:53 +0430

    Add Styled Loading

---
+++ src/Components/Loading/Styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(255, 255, 255, 0.3);
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export { Container };
