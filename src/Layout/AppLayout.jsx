d4e33da4ff4ab444f8476b34c21982a2b349f64f
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-09 01:22:09 +0430

    Refactoer App Layout

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -1,39 +1,22 @@
 import React, { useState } from "react";
-import { Layout, Menu } from "antd";
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
-import { Logo, StyledLayout, Trigger, StyledHeader } from "./Styled";
-import menu from "Constant/menu";
-import { useLocation } from "react-router";
-import { Link } from "react-router-dom";
+import { Layout } from "antd";
+import { StyledLayout } from "./Styled";
 
-const { Sider, Content } = Layout;
+import MainSider from "Container/MainSider/MainSider";
+import MainHeader from "Container/MainHeader/MainHeader";
+
+const { Content } = Layout;
 
 function AppLayout({ children }) {
-  const location = useLocation();
   const [collapsed, setCollapsed] = useState(false);
   const toggle = () => setCollapsed((prevCollapsed) => !prevCollapsed);
-  const TriggerIcon = !collapsed ? MenuUnfoldOutlined : MenuFoldOutlined;
 
   return (
     <StyledLayout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <Logo />
-        <Menu theme="dark" mode="inline" selectedKeys={[location.pathname]}>
-          {menu.map((item) => (
-            <Menu.Item key={item.key} icon={item.icon}>
-              <Link to={item.key}>{item.label}</Link>
-            </Menu.Item>
-          ))}
-        </Menu>
-      </Sider>
+      <MainSider collapsed={collapsed} />
       <Layout className="site-layout">
-        <StyledHeader>
-          <Trigger onClick={toggle}>
-            <TriggerIcon />
-          </Trigger>
-        </StyledHeader>
+        <MainHeader toggle={toggle} collapsed={collapsed} />
         <Content
-          className="site-layout-background"
           style={{
             padding: 24,
             minHeight: 280,
--- src/Layout/Styled.js
+++ src/Layout/Styled.js
@@ -1,32 +1,8 @@
-import { Layout, PageHeader } from "antd";
+import { Layout } from "antd";
 import styled from "styled-components";
-
-const { Header } = Layout;
 
 const StyledLayout = styled(Layout)`
   height: 100vh;
 `;
 
-const Trigger = styled.div`
-  padding: 0 24px;
-  font-size: 18px;
-  line-height: 64px;
-  cursor: pointer;
-  transition: color 0.3s;
-  &:hover {
-    color: #1890ff;
-  }
-`;
-
-const Logo = styled.div`
-  height: 32px;
-  margin: 16px;
-  background: rgba(255, 255, 255, 0.3);
-`;
-
-const StyledHeader = styled(Header)`
-  background: #fff;
-  padding: 0;
-`;
-
-export { StyledLayout, Trigger, Logo, StyledHeader };
+export { StyledLayout };
