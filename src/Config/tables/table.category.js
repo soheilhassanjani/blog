79bade21e14b93f80f92942f2ac279e0c0722901
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-08 04:40:04 +0430

    Add Config TABLE_CATEGORY

---
+++ src/Config/tables/table.category.js
@@ -0,0 +1,30 @@
+import NoWrapText from "Components/NoWrapText";
+
+const TABLE_CATEGORY = [
+  {
+    title: "name",
+    dataIndex: "name",
+    key: "name",
+    render: (value) => {
+      return <NoWrapText>{value || "بدون داده"}</NoWrapText>;
+    },
+  },
+  {
+    title: "parentCategoryName",
+    dataIndex: "parentCategoryName",
+    key: "parentCategoryName",
+    render: (value) => {
+      return <NoWrapText>{value || "بدون داده"}</NoWrapText>;
+    },
+  },
+  {
+    title: "parentCategoryId",
+    dataIndex: "parentCategoryId",
+    key: "parentCategoryId",
+    render: (value) => {
+      return <NoWrapText>{value || "بدون داده"}</NoWrapText>;
+    },
+  },
+];
+
+export default TABLE_CATEGORY;
