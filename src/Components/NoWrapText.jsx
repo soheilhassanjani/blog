89da9455a742598187e264b681608c8f0256c175
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-08 04:39:37 +0430

    Add NoWrapText

---
+++ src/Components/NoWrapText.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const NoWrapText = styled.div`
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export default NoWrapText;
