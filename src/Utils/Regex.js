ddf45c45cbe077dd2aa76d7b316d31ffb85659d9
Soheil Hassanjani <soheilpcmir@gmail.com>
2021-06-09 01:15:51 +0430

    Add Regex

---
+++ src/Utils/Regex.js
@@ -0,0 +1,12 @@
+function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validatePassword(password) {
+  const length = password.length;
+  return length >= 6 && length <= 12;
+}
+
+export { validateEmail, validatePassword };
